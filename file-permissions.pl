844f82409635c1888e76b35e63559914155c9b54
Ben Brandt <benbrandt@gmail.com>
2016-11-05 14:51:56 -0500

    learn to create a module

--- file-permissions.pl
+++ file-permissions.pl
@@ -6,6 +6,8 @@ use Fun::Files::Iterate;
 
 iterate 
 	'.', 
-	sub { (stat $_[0])[2] & S_IWOTH },
-	sub { chmod o-w $_[0] }
+	#sub { (stat $_[0])[2] & S_IWOTH },
+	sub { return 1; },
+	#sub { chmod o-w $_[0] }
+	sub { print $_[0]; }
 
